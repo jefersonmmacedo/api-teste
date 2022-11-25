3ac835b9ead4be86cea3bbe4dbe51f3bc41fd80a
jefersonmmacedo <jefersonmacedowgf@gmail.com>
2022-11-25 12:06:11 -0300

    TESTE

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
 import { server } from "./http";
 import "./websocket"
 
-const port = process.env.PORT || 3333;
+// const port = process.env.PORT || 3333;
+const port = 3333;
 
 server.listen(port, () => {
   console.log(`Server initialized! Access the link: http://localhost:${port}`);
